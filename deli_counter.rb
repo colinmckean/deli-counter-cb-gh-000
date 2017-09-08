9a9296187337e70eea548ef6186c9d7302d8de81
Colin McKean <ezikill@hotmail.com>
2017-09-08 15:19:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,9 +10,9 @@ def line(thing)
   end
 end
 
-def take_a_number(thing1, thing2)
-  thing1.push(thing2)
-  puts "Welcome, #{thing2}. You are number #{thing1.index(thing2)+1} in line."
+def take_a_number(deli, name)
+  thing1.push(name)
+  puts "Welcome, #{name}. You are number #{deli.index(name)+1} in line."
 end
 
 def now_serving(otherthing)
